74699b7807a6c8cb234cd496342279e55c9f0aa5
mkotsollaris <mkotsollari@gmail.com>
2022-11-18 15:09:26 -0800

    init

--- snippets/no-amd-library.js
+++
@@ -1,1 +0,0 @@
-https://js.appboycdn.com/web-sdk/4.4/braze.no-amd.min.js
--- snippets/service-worker-skip-waiting.js
+++
@@ -1,4 +0,0 @@
-self.addEventListener("install", event => {
-  event.stopImmediatePropagation();
-});
-self.importScripts("https://js.appboycdn.com/web-sdk/4.4/service-worker.js");
